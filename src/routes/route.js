1db7b6110ec88ce1354e8d186467605b7be7a5e4
sohelrana1304 <sohelrana1304@gmail.com>
2022-04-08 00:21:19 +0530

    one done

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,13 +4,17 @@ const logger = require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
+    // console.log('------------------')
+    // console.log(req)
+    // console.log('------------------')
+    // console.log('These are the request query parameters: ', req.query)
+
+    let candidatesList = ["Sohel", "Rana", "Wasim", "Ripon", "Galib", "Rahul"]
+    console.log('Candidates are:', candidatesList)
+
+
     res.send('My first ever api!')
 });
-
 
 
 
